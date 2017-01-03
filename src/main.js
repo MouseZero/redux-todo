5903fb738f109161c904f78687c5133a7d0e9409
mousezero <rm@mousezero.com>
2017-01-02 19:58:52 -0800

    stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,62 @@
-console.log('Hello There');
+//----React
+function addTodoButton(){
+  store.dispatch({
+    type: 'ADD_TODO',
+    id: counter++,
+    text: 'my message',
+    isChecked: false
+  });
+}
 
+function ShowTodos({todos}){
+  console.log(todos);
+  return (
+    <div>
+      Hello There
+    </div>
+  );
+}
+
+function Buttons(){
+  return(
+    <div>
+      <button onClick={addTodoButton}>Add Todo</button>
+    </div>
+  );
+}
+
+//----Redux
+let counter = 0;
+//- Reducer
+function reducer(state = {todos: []}, action){
+  const {id, text, isChecked} = action
+  switch(action.type){
+    case 'ADD_TODO':
+      return Object.assign(
+        {}, 
+        state, 
+        {
+          todos: [...state.todos, {
+            id, text, isChecked
+          }]
+        }
+      );
+    default:
+      return state
+  }
+}
+
+const store = Redux.createStore(reducer)
+store.subscribe(render);
+
+//-----Render
 function render(){
   ReactDOM.render(
     <div>
-      Hello there
+      <ShowTodos todos={store.getState().todos}/>
+      <Buttons/>
     </div>,
     document.getElementById('root')
-  )
+  );
 }
-
 render();
